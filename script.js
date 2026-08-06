9285cb53315ed6116e0f75ed8ee68c5749927ad3
MGobronidze <gobronidzemariami@gmail.com>
2026-08-06 16:09:26 +0400

    adding modified 5th session

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ const LESSONS = [
   { n: 3, mod: "html", title: "HTML სიები, სურათები, ფორმები და მედია" },
 
   { n: 4, mod: "css", title: "CSS სინტაქსი, სტრუქტურული ტეგები, Class/ID, Normalize" },
-  { n: 5, mod: "css", title: "Display თვისებები, Background, ტექსტის ფორმატირება" },
+  { n: 5, mod: "css", title: "Display, Background, ტექსტი, ვებ-ფონტები, Figma" },
   { n: 6, mod: "css", title: "CSS Flexbox და Positioned Elements" },
   { n: 7, mod: "css", title: "Pseudo-classes, Transitions, Animations, Variables, Autoprefixer" },
   { n: 8, mod: "css", title: "Bootstrap, Git & GitHub, რესპონსიული დიზაინი (Media Queries)" },
